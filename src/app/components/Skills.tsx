f891aaf4afde1cd55724b52dff8f96232711f3ca
PrathamTelang <prathamtelang007@gmail.com>
2025-10-10 19:54:55 +0530

    Update skills component and assets
    Replaced LinkedIn SVG icon with a motion.png image in the skills component. Updated dependencies in package.json and removed the unused LinkdinIcon.tsx file.

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,6 +1,14 @@
-import { ArrowUpRight } from "lucide-react"
+import { FaGitAlt, FaNodeJs, FaReact } from "react-icons/fa"
 import { Line } from "./Line"
-import { LinkedInIcon } from "./svg/LinkdinIcon"
+import { SiExpress, SiJavascript, SiMongodb, SiPostgresql, SiTailwindcss, SiTypescript } from "react-icons/si"
+import { RiNextjsFill } from "react-icons/ri"
+import { VscVscode } from "react-icons/vsc"
+import motionIcon from "./svg/Motion.svg"
+import motionIconpng from "./images/motion.png"
+import Image from "next/image"
+
+
+
 
 export const Skills = () => {
     return (
@@ -8,8 +16,33 @@ export const Skills = () => {
             <div className="w-3/5  border-x-2 border-[#E5E5E5] max-[475px]:w-screen">
                 <h2 className="text-2xl px-4 font-semibold ">Techstack</h2>
                 <Line/>
-                
-                
+                <div className="pt-2">
+                    <h3 className="text-lg px-4 font-semibold">Frontend</h3>
+                    <div className="flex pl-4 text-4xl gap-4">
+                        <SiTypescript className="text-[#007ACC]" />
+                        <SiJavascript className="text-[#F7DF1E]"/>
+                        <FaReact />
+                        <RiNextjsFill />
+                        <SiTailwindcss />
+                    </div>
+                </div>
+                <div className="pt-2">
+                    <h3 className="text-lg px-4 font-semibold">Backend</h3>
+                    <div className="flex pl-4 text-4xl gap-4">
+                        <FaNodeJs />
+                        <SiExpress />
+                        <SiMongodb />
+                        <SiPostgresql />
+                    </div>
+                </div>
+                <div className="pt-2">
+                    <h3 className="text-lg px-4 font-semibold">Design and Tools</h3>
+                    <div className="flex pl-4 text-4xl gap-4">
+                        <FaGitAlt />
+                        <VscVscode />
+                        <Image src={motionIconpng} alt="motion" width={40} height={40}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
