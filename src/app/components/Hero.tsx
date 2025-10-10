e27a10ff7cff4fe696896fa0fc1ecc29e225f71e
PrathamTelang <prathamtelang007@gmail.com>
2025-10-11 01:39:10 +0530

    Add Indian flag image with tooltip to Hero component
    Introduces an Indian flag image to the Hero component with a tooltip that appears on hover, indicating 'I'm from India'. Also adds the flag image asset to the project.

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,12 +3,27 @@ import pfp from "./images/pfp.png"
 import shinchan from "./images/shinchan.jpeg"
 import { Line } from "./Line"
 import thanos from "./images/thanos.jpg"
+import IndianFlag from "./images/Flag_of_India.svg.png"
 
 export const Hero = () => {
     return (
         <div className="w-screen  flex justify-center">
             <div className="flex border-x border-[#E5E5E5] w-3/5 max-[475px]:w-screen max-[475px]:max-h-36 max-h-full">
                 <div className="h-48 min-w-48 max-[475px]:min-w-36 max-[475px]:min-h-full">
+                    <div className="absolute group inline-block ">
+  <Image
+    alt="Indian Flag"
+    src={IndianFlag}
+    className="h-12 w-auto max-[475px]:h-8"
+  />
+  <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 
+                  opacity-0 group-hover:opacity-100 
+                  transition-opacity duration-300 
+                  bg-gray-800 text-white text-sm rounded py-1 px-2 whitespace-nowrap pointer-events-none">
+    I'm from India
+  </div>
+</div>
+
                     <Image
                         src={thanos} 
                         alt="Dark Hole" 
@@ -32,3 +47,4 @@ export const Hero = () => {
         </div>
     )
 }
+
