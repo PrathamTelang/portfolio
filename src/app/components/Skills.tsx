ab1272415d5cd8ef9e608e68ac8b3d2fe6f84057
PrathamTelang <prathamtelang007@gmail.com>
2025-10-10 21:57:19 +0530

    Update background styles in Backgroud and Skills components
    Swapped the background grid effect between Backgroud.tsx and Skills.tsx. Backgroud.tsx now uses a linear gradient grid with pseudo-elements, while Skills.tsx uses a radial gradient grid. This improves visual consistency and separation between the two sections.

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -16,13 +16,10 @@ export const Skills = () => {
             <div className="w-3/5  border-x-2 border-[#E5E5E5] max-[475px]:w-screen ">
                 <h2 className="text-2xl px-4 font-semibold ">Techstack</h2>
                 <Line/>
-                <div className="relative overflow-hidden 
-before:absolute before:inset-0 before:content-[''] before:z-0
-  before:bg-[linear-gradient(to_right,rgba(0,0,0,0.05)_1px,transparent_1px)]
-  after:absolute after:inset-0 after:content-[''] after:z-0
-  after:bg-[linear-gradient(to_bottom,rgba(0,0,0,0.05)_1px,transparent_1px)]
-  before:bg-[size:4px_4px] after:bg-[size:4px_4px]">
-                    <div className="pt-2">
+                <div className=" bg-[radial-gradient(circle,_#E5E5E5_1px,_transparent_1px)] 
+                      bg-[length:10px_10px]">
+                    <div className="relative z-10">
+                        <div className="pt-2 ">
                     <h3 className="text-lg px-4 font-semibold">Frontend</h3>
                     <div className="flex items-center pl-4 text-4xl gap-4">
                         <SiTypescript className="text-[#007ACC]" />
@@ -49,6 +46,7 @@ before:absolute before:inset-0 before:content-[''] before:z-0
                         <Image src={motionIconpng} alt="motion" width={40} height={40} className="object-contain p-1"/>
                     </div>
                 </div>
+                    </div>
                 </div>
             </div>
         </div>
