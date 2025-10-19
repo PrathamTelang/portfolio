85956cb4150685682d014666a312f30f70fc6960
PrathamTelang <prathamtelang007@gmail.com>
2025-10-19 22:47:58 +0530

    Add dark mode styles to Background component
    Introduced dark mode support for the Background component by adding dark theme classes and updating background gradients to use CSS variables for light and dark borders.

--- src/app/components/Backgroud.tsx
+++ src/app/components/Backgroud.tsx
@@ -2,12 +2,13 @@ import '@/app/globals.css'
 
 export const Background = () => {
   return (
-    <div className="flex w-screen pt-12  items-center justify-center">
-      <div className=" w-3/5 border-x border-lightborder max-[475px]:w-11/12">
+    <div className="flex w-screen pt-12  items-center justify-center ">
+      <div className=" w-3/5 border-x border-lightborder dark:bg-darkbg dark:border-darkborder dark:text-darkprimary-text max-[475px]:w-11/12">
       <div className="w-full h-60 flex justify-center items-center
-                      relative overflow-hidden bg-white
-bg-[linear-gradient(to_right,rgba(0,0,0,0.02)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.02)_1px,transparent_1px)]
+                      relative overflow-hidden bg-white dark:bg-darkbg
+bg-[linear-gradient(to_right,var(--color-lightborder)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-lightborder)_1px,transparent_1px)]
 bg-[size:4px_4px]
+dark:bg-[linear-gradient(to_right,var(--color-darkborder)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-darkborder)_1px,transparent_1px)]
       
                       ">
         <div className="flex items-center justify-center text-8xl font-extrabold ">
