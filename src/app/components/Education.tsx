e6812a4670a46f00f06f0f9f7d239b7ce0f28708
PrathamTelang <prathamtelang007@gmail.com>
2025-10-24 19:44:29 +0530

    Update Education section styling and layout
    Refactored the Education component to use an absolutely positioned, rotated section title for improved visual design. Adjusted padding and removed the Line component for a cleaner layout.

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -3,10 +3,11 @@ import { Line } from "./Line"
 export const Education = () => {
     return (
         <div className="w-screen flex justify-center ">
-            <div className="w-3/5 bg-white dark:bg-darkbg border-x border-lightborder dark:border-darkborder max-[475px]:w-11/12">
-            <h2 className="text-2xl px-4 font-semibold  dark:text-darkprimary-text">Education</h2>
-            <Line/>
-            <div className="pt-2">
+            <div className="w-3/5 relative bg-white dark:bg-darkbg border-x border-lightborder dark:border-darkborder max-[475px]:w-11/12">
+            <h2 className="text-xs font-semibold text-[#FF8243] 
+           transform -rotate-90  absolute sm:top-8 sm:-left-13 sm:text-lg top-5 -left-8.5
+        ">Education</h2>
+            <div className="py-2">
                 <h3 className="text-lg px-4 font-semibold dark:text-darkprimary-text">Raisoni University</h3>
             <p className="text-secondary-text px-4 mt-1 text-md dark:text-darksecondary-text">
                 Bachelor of Computer Application
