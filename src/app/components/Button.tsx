015acb5561f6308211e66bafb38f0c2ee36699cd
PrathamTelang <prathamtelang007@gmail.com>
2025-11-19 22:26:56 +0530

    Refactor Button to use animated SVG corners
    Replaces static FaPlus corner icons with animated SVGs using Framer Motion for a more dynamic button appearance. Changes the root element from a div to an anchor tag and updates styling for improved visual effects.

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { motion } from "motion/react";
+
 
 export interface ButtonProps {
     text: string;
@@ -13,18 +15,61 @@ export interface ButtonProps {
 
 function Button(props: ButtonProps) {
   return (
-    <div 
+    <a
     onClick={props.onClick}
-    style={{ cursor: props.cursor }}
-    className='text-primary  dark:bg-[#03192A]  dark:hover:bg-sky-400/20 px-2 w-fit border border-dashed border-sky-300/60 bg-sky-400/10 hover:bg-sky-400/15 dark:border-sky-300/30 relative '>
-        <FaPlus className='h-1.5 w-1.5 absolute -top-1/7 -left-1/18 text-[#73D4FF] dark:text-[#3B6E89]' />
-        <FaPlus className='h-1.5 w-1.5 absolute -top-1/7 -right-1/18 text-[#73D4FF] dark:text-[#3B6E89]' />
-        <FaPlus className='h-1.5 w-1.5 absolute -bottom-1/7 -left-1/18 text-[#73D4FF] dark:text-[#3B6E89]' />
-        <FaPlus className='h-1.5 w-1.5 absolute -bottom-1/7 -right-1/18 text-[#73D4FF] dark:text-[#3B6E89]' />
-        {props.startIcon}
+    style={{ cursor: props.cursor }} 
+    className="group relative px-1.5 text-lg text-sky-800 dark:text-sky-300">
+      <span className="absolute inset-0 border border-dashed border-sky-300/60 bg-sky-400/10 group-hover:bg-sky-400/15 dark:border-sky-300/30">
+      </span>
+    {props.startIcon}
     {props.text}
     {props.endIcon}
-    </div>
+    <motion.svg 
+      width="5" 
+      height="5" 
+      viewBox="0 0 5 5" 
+      className="absolute top-[-2px] left-[-2px] fill-sky-300 dark:fill-sky-300/50 drop-shadow-2xl drop-shadow-sky-400/20 dark:drop-shadow-sky-900/20
+
+"
+      animate={{ opacity: [0, 1, 0] }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut"
+      }}
+      >
+        <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
+    </motion.svg>
+    <motion.svg 
+    width="5" 
+    height="5" 
+    viewBox="0 0 5 5" 
+    className="absolute top-[-2px] right-[-2px] fill-sky-300 dark:fill-sky-300/50" 
+    animate={{ opacity: [0, 1, 0] }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut"
+      }}>
+        <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
+      </motion.svg>
+    <motion.svg 
+    width="5" 
+    height="5" 
+    viewBox="0 0 5 5" 
+    className="absolute bottom-[-2px] left-[-2px] fill-sky-300 dark:fill-sky-300/50" 
+    animate={{ opacity: [0, 1, 0] }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut"
+      }}><path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path></motion.svg>
+    <motion.svg width="5" height="5" viewBox="0 0 5 5" className="absolute right-[-2px] bottom-[-2px] fill-sky-300 dark:fill-sky-300/50" animate={{ opacity: [0, 1, 0] }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        ease: "easeInOut"
+      }}><path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path></motion.svg></a>
   )
 }
 
