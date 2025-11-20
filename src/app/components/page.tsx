ed746954743cd0fd30b013ec2cb432ea10d75404
PrathamTelang <prathamtelang007@gmail.com>
2025-11-21 04:31:48 +0530

    Redesign ComponentsPage layout and add Line component
    Updated the ComponentsPage with a new layout, background styles, and grid structure. Integrated the Line component before and after each component entry for improved visual separation.

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,5 +1,6 @@
 // src/app/components/page.tsx
 import Link from "next/link";
+import { Line } from "../features/portfolio/components/Line";
 
 const components = [
   { name: "Button", slug: "button" },
@@ -11,21 +12,28 @@ const components = [
 
 export default function ComponentsPage() {
   return (
-    <div className="min-h-screen p-10">
-      <h1 className="text-4xl font-bold mb-8">Components</h1>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {components.map((c) => (
+    <div className="max-w-screen tracking-tighter text-balance overflow-x-hidden ">
+    <div className="h-full max-w-screen bg-[#FCFCFC] bg-[repeating-linear-gradient(45deg,var(--color-lightborder)_0_1px,transparent_1px_8px)]
+    dark:bg-darkbg dark:bg-[repeating-linear-gradient(45deg,var(--color-darkborder)_0_1px,transparent_1px_8px)]">
+      <div className="relative isolate mt-16 bg-lightbg dark:bg-darkbg">
+        <div className="pointer-events-none absolute inset-0 z-10 grid grid-cols-2 gap-10 max-md:gap-5 lg:grid-cols-3 xl:grid-cols-4"><div className="border-r border-gray-950/5 dark:border-white/10"></div><div className="border-l border-gray-950/5 lg:border-x dark:border-white/10"></div><div className="border-l border-gray-950/5 max-lg:hidden xl:border-x dark:border-white/10"></div><div className="border-l border-gray-950/5 max-xl:hidden dark:border-white/10"></div>
+        </div>
+        <ul className="grid grid-cols-2 gap-5 md:gap-10 lg:grid-cols-3 xl:grid-cols-4">
+          {components.map((c) => (
           <Link
             key={c.slug}
             href={`/components/${c.slug}`}
-            className="border rounded-xl p-6 hover:bg-neutral-900 transition"
+            className=" hover:bg-neutral-900 transition"
           >
-            <h2 className="text-xl font-semibold">{c.name}</h2>
-            <p className="text-neutral-400">View component →</p>
+            <Line/>
+            <h2 className="text-xl font-semibold pl-4">{c.name}</h2>
+            <p className="text-neutral-400 pl-4">View component →</p>
+            <Line/>
           </Link>
         ))}
-      </div>
+    </ul>
+    </div>
+    </div>
     </div>
   );
 }
