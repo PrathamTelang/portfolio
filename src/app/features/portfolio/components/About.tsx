fd1cbe94d2a6a7eaf337b437766e3b12aaa26c82
PrathamTelang <prathamtelang007@gmail.com>
2025-12-16 21:22:56 +0530

    Refactor About component and update CSS variables
    Simplified About component styles by consolidating background and border color variables. Removed redundant dark/light theme variables from globals.css and ensured consistent use of CSS custom properties for theming.

--- src/app/features/portfolio/components/About.tsx
+++ src/app/features/portfolio/components/About.tsx
@@ -1,16 +1,15 @@
 
 export const About = () => {
   return (
-    <div className="w-screen flex justify-center items-center bg-lightbg dark:bg-darkbg bg-[repeating-linear-gradient(45deg,var(--color-lightborder)_0_1px,transparent_1px_8px)]
-     dark:bg-[repeating-linear-gradient(45deg,var(--color-darkborder)_0_1px,transparent_1px_8px)]">
-      <div className="w-11/12 lg:w-3/5 relative bg-white border-x border-lightborder dark:bg-darkbg dark:border-darkborder  
+    <div className="w-screen flex justify-center items-center bg-background bg-[repeating-linear-gradient(45deg,var(--color-border)_0_1px,transparent_1px_8px)]">
+      <div className="w-11/12 lg:w-3/5 relative border-x bg-background border-border
       ">
           <h2 className="text-xs font-semibold text-[#615FFF] 
            transform -rotate-90  absolute sm:top-7 sm:-left-12 sm:text-lg top-5 -left-7.5
         ">About Me</h2>
         
         
-        <p className="text-secondary-text px-4 my-2 text-md dark:text-darksecondary-text">
+        <p className="text-secondary-text px-4 my-2 text-md ">
           Pratham Telang <br/>
 Web Developer & Design Engineer <br/>
 I build fast, intuitive, and user-centric digital experiences using modern web technologies like React, Next.js, and TypeScript.
