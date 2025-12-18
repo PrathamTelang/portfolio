a6b82e4a293d691961478e3184dfb2ca539c03da
PrathamTelang <prathamtelang007@gmail.com>
2025-12-18 13:56:57 +0530

    Fix formatting in About component paragraph
    Adjusted line breaks and indentation in the About component's paragraph for improved readability and consistency.

--- src/app/features/portfolio/components/About.tsx
+++ src/app/features/portfolio/components/About.tsx
@@ -10,9 +10,11 @@ export const About = () => {
         
         
         <p className="text-secondary-text px-4 my-2 text-md ">
-          Pratham Telang <br/>
-Web Developer & Design Engineer <br/>
-I build fast, intuitive, and user-centric digital experiences using modern web technologies like React, Next.js, and TypeScript.
+          Pratham Telang 
+          <br/>
+          Web Developer & Design Engineer 
+          <br/>
+          I build fast, intuitive, and user-centric digital experiences using modern web technologies like React, Next.js, and TypeScript.
         </p>
       </div>
     </div>
