b741998d294afc5584793227eb0b46b32a8cf545
PrathamTelang <prathamtelang007@gmail.com>
2026-01-26 22:30:59 +0530

    Replace Email button with navigation links in Navbar
    Removed the Email button from the Navbar and added navigation links to 'Portfolio' and 'Components' using Next.js Link. This improves navigation within the application.

--- src/app/features/portfolio/components/Navbar.tsx
+++ src/app/features/portfolio/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { BsMoonStars } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { MdOutlineLightMode } from "react-icons/md";
 import Button from "./Button";
+import Link from "next/link";
 
 export const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -43,13 +44,13 @@ export const Navbar = () => {
   return (
     <div className="flex w-screen justify-center h-12 fixed top-0 left-0 border-y border-border z-50 bg-background ">
       <div className="w-11/12 lg:w-3/5 border-x border-border flex items-center justify-end px-4 gap-2">
-      <div>
-            <Button
-              text="Email"
-              fontSize="20px"
-              variant="primary"
-              onClick={() => window.open("https://mail.google.com/mail/?view=cm&fs=1&to=prathamtelang007@gmail.com", "_blank")} 
-            />
+      <div className="flex justify-center items-center gap-4 font-semibold">
+        <Link href="/" >
+              <div>Portfolio</div>
+            </Link>
+            <Link href="/components" >
+              <div>Components</div>
+            </Link>
           </div>
       <div className="cursor-pointer rounded-full border border-border p-2 flex items-center justify-center hover:bg-border "
         onClick={() => window.open("https://github.com/PrathamTelang/portfolio")}
